a215b823438734ecfcaaf3199796a0c2f107d78c
ruta-kazenaite <62906116+ruta-kazenaite@users.noreply.github.com>
2020-03-30 23:31:10 +0300

    Update Ateities pranasyste.py

--- Ateities pranasyste.py
+++ Ateities pranasyste.py
@@ -3,32 +3,65 @@ age = input ('How old are you? ')
 sex = input ('Are you male or female? ')
 answer = input ('Do you want to know your future - answer yes or no? ')
 if answer.startswith('y') or answer.startswith('Y') :
-    print('Ok. Where should I start...')
+    from time import sleep
+    for i in range(27):
+        print('Ok. Where should I start...'[i], sep='', end='', flush=False);
+        sleep(0.1)
     if int(age) < 18:
-        print('You will live a long life. Everything is ahead of you.')
+        for i in range(55):
+            print('\nYou will live a long life. Everything is ahead of you.'[i], sep='', end='', flush=False);
+            sleep(0.1)
     if 18 < int(age) < 50:
-        print('You are entering the middle of your life.')
+        for i in range(42):
+            print('\nYou are entering the middle of your life.'[i], sep='', end='', flush=False);
+            sleep(0.1)
     if int(age) > 50:
-        print('You are in the second half of your life. Enjoy it while you can.')
+        for i in range(65):
+            print('\nYou are in the second half of your life. Enjoy it while you can.'[i], sep='', end='', flush=False);
+            sleep(0.1)
     if sex.startswith('m') or sex.startswith('M'):
         if int(age) < 18:
-            print('You will get into a lot of trouble.')
+            for i in range(36):
+                print('\nYou will get into a lot of trouble.'[i], sep='', end='', flush=False);
+                sleep(0.1)
         if 18 < int(age) < 50:
-            print('You will be very rich and very bored...')
+            for i in range(39):
+                print('\nYou will be very rich and very bored...'[i], sep='', end='', flush=False);
+                sleep(0.1)
         if int(age) > 50:
-            print('You will start fishing.')
+            for i in range(24):
+                print('\nYou will start fishing.'[i], sep='', end='', flush=False);
+                sleep(0.1)
     if sex.startswith('f') or sex.startswith('F'):
         if int(age) < 18:
-            print('You will make a lot of friends.')
+            for i in range(32):
+                print('\nYou will make a lot of friends.'[i], sep='', end='', flush=False);
+                sleep(0.1)
         if 18 < int(age) < 50:
-            print('You will find love, and lose it. And find it again. And lose it again.')
+            for i in range(71):
+                print('\nYou will find love, and lose it. And find it again. And lose it again.'[i], sep='', end='', flush=False);
+                sleep(0.1)
         if int(age) > 50:
-            print('You will rediscover yourself.')
-    if name[0] < 'N':
-        print('Oh, and fun fact - your name\'s first letter is between A-M')
+            for i in range(30):
+                print('\nYou will rediscover yourself.'[i], sep='', end='', flush=False);
+                sleep(0.1)
+    if name[0] < 'N' or name[0] < 'n':
+        for i in range(59):
+            print('\nOh, and fun fact - your name\'s first letter is between A-M'[i], sep='', end='', flush=False);
+            sleep(0.1)
     else:
-        print('Oh, and fun fact - your name\'s first letter is between N-Z')
-    print('Did you know your name starts with ' + name[0] + "? " +
-    'It means you\'re lucky!')
+        for i in range(59):
+            print('\nOh, and fun fact - your name\'s first letter is between N-Z'[i], sep='', end='', flush=False);
+            sleep(0.1)
+    for i in range(36):
+        print('\nDid you know your name starts with '[i], sep='', end='', flush=False);
+        sleep(0.1)
+    print(name[0] + "? ")
+    for i in range(22):
+        print('It means you\'re lucky!'[i], sep='', end='', flush=False);
+        sleep(0.1)
 else:
-    print('Your loss...')
+    from time import sleep
+    for i in range(13):
+        print('\nYour loss...'[i], sep='', end='', flush=False);
+        sleep(0.1)
